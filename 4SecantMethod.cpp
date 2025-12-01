165fbd625621aedc981eb20051b83c7ac5ba1c15
samiranjumma <samiran.jumma@gmail.com>
2025-12-01 08:23:16 +0600

    Update 4SecantMethod.cpp

--- 4SecantMethod.cpp
+++ 4SecantMethod.cpp
@@ -1,72 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define tolerance 1e-5
 
-class Secant {
+class NewtonRaphson{
 private:
-    double x0, x1;  // two initial guesses
-
+    double a;
+    double tolerance;
 public:
-    Secant() {
-        randomNumberGenerator();
+    NewtonRaphson(double fi, double tol) : a(fi),tolerance(tol){}
+
+    double calculateValue(double x) {
+        return pow(x, 2) - 2 * x - 4;
+    }
+    double DifferentionValue(double x) {
+        return 2 * x - 2;
     }
 
-    // Example function: f(x) = x^3 - 3x - 1
-    double function(double x) {
-        return x * x * x - 3 * x - 1;
-    }
-
-    // Generate two random distinct guesses
-    void randomNumberGenerator() {
-        srand(time(0));
-        do {
-            x0 = rand() % 201 - 100;  // between -100 and 100
-            x1 = rand() % 201 - 100;
-        } while (x0 == x1);
-    }
-
-    // Secant Method Iteration
-    void findRoot() {
-        double f0 = function(x0), f1 = function(x1);
-        int iter = 0;
-
-        while (fabs(x1 - x0) >= tolerance) {
-            if (fabs(f1 - f0) < 1e-12) { // avoid division by zero
-                cout << "Division by zero risk. Stopping iteration.\n";
-                return;
-            }
-
-            double x2 = x1 - f1 * (x1 - x0) / (f1 - f0);
-
-            cout << "Iteration " << ++iter << ": x = " << x2 << '\n';
-
-            if (fabs(function(x2)) < tolerance) {
-                cout << "The root is approximately: " << x2 << '\n';
-                return;
-            }
-
-            // Shift values for next iteration
-            x0 = x1;
-            f0 = f1;
-            x1 = x2;
-            f1 = function(x1);
+    int i = 1;
+    void Iteration() {
+        double x = a - (calculateValue(x) / DifferentionValue(x));
+        while(fabs(a-x) > tolerance) {
+            a = x;
+            x = a - (calculateValue(x) / DifferentionValue(x));
+            printf("The New Value of x%d = %.6f\n", i, x);
+            i++;
         }
 
-        cout << "The root is approximately: " << x1 << '\n';
-    }
-
-    void equation() {
-        cout << "Equation: x^3 - 3x - 1" << '\n';
-    }
-
-    void displayInitialGuesses() {
-        cout << "Initial guesses: x0 = " << x0 << ", x1 = " << x1 << '\n';
+        printf("The Final Root is = %f\n" ,x);
     }
 };
 
 int main() {
-    Secant sec;
-    sec.displayInitialGuesses();
-    sec.findRoot();
-    return 0;
+    srand((unsigned) time(NULL));
+
+    double a = rand()%10 - 5;
+    double tolerance = 0.0001;
+
+    cout << "a = " << a << '\n';
+
+    NewtonRaphson nwtr = NewtonRaphson(a, tolerance);
+
+    nwtr.Iteration();
 }
