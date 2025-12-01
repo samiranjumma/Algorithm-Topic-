699107a4097c0316052837b6452cbf47e3dcae25
samiranjumma <samiran.jumma@gmail.com>
2025-12-01 08:23:40 +0600

    Update 3NewtonRapshonMethod.cpp

--- 3NewtonRapshonMethod.cpp
+++ 3NewtonRapshonMethod.cpp
@@ -1,71 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define tolerance 1e-5
 
-class NewtonRaphson {
+class NewtonRaphson{
 private:
-    double x0;  // initial guess
-
+    double a;
+    double tolerance;
 public:
-    NewtonRaphson() {
-        randomNumberGenerator();
+    NewtonRaphson(double fi, double tol) : a(fi),tolerance(tol){}
+
+    double calculateValue(double x) {
+        return pow(x, 2) - 2 * x - 4;
+    }
+    double DifferentionValue(double x) {
+        return 2 * x - 2;
     }
 
-    // Example function: f(x) = x^3 - 3x - 1
-    double function(double x) {
-        return x * x * x - 3 * x - 1;
-    }
-
-    // Derivative: f'(x) = 3x^2 - 3
-    double derivative(double x) {
-        return 3 * x * x - 3;
-    }
-
-    // Generate random initial guess
-    void randomNumberGenerator() {
-        srand(time(0));
-        x0 = rand() % 201 - 100;  // random between -100 and 100
-    }
-
-    // Newton-Raphson Iteration
-    void findRoot() {
-        double x = x0;
-        int iter = 0;
-
-        while (fabs(function(x)) >= tolerance) {
-            double d = derivative(x);
-            if (d == 0) { // avoid division by zero
-                cout << "Derivative is zero. Stopping iteration.\n";
-                return;
-            }
-
-            double x1 = x - function(x) / d;
-
-            cout << "Iteration " << ++iter << ": x = " << x1 << '\n';
-
-            if (fabs(x1 - x) < tolerance) {
-                cout << "The root is approximately: " << x1 << '\n';
-                return;
-            }
-
-            x = x1;
+    int i = 1;
+    void Iteration() {
+        double x = a - (calculateValue(x) / DifferentionValue(x));
+        while(fabs(a-x) > tolerance) {
+            a = x;
+            x = a - (calculateValue(x) / DifferentionValue(x));
+            printf("The New Value of x%d = %.6f\n", i, x);
+            i++;
         }
 
-        cout << "The root is approximately: " << x << '\n';
-    }
-
-    void equation() {
-        cout << "Equation: x^3 - 3x - 1" << '\n';
-    }
-
-    void displayInitialGuess() {
-        cout << "Initial guess: " << x0 << '\n';
+        printf("The Final Root is = %f\n" ,x);
     }
 };
 
 int main() {
-    NewtonRaphson nr;
-    nr.displayInitialGuess();
-    nr.findRoot();
-    return 0;
+    srand((unsigned) time(NULL));
+
+    double a = rand()%10 - 5;
+    double tolerance = 0.0001;
+
+    cout << "a = " << a << '\n';
+
+    NewtonRaphson nwtr = NewtonRaphson(a, tolerance);
+
+    nwtr.Iteration();
 }
